8982fc22d611aa661c25d7754ae8ed5f5e34c9b4
inakiAndres <inaki.andres.beltran@accenture.com>
2020-11-02 13:59:03 +0100

    -Added responsive design -Gap size reduced when level up -Added some text of color behavour to readme

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,37 +3,63 @@ import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
   cards: {
-    width: "50vw",
-    height: "50vw",
+    width: "40vw",
+    height: "40vw",
     margin: "0 auto",
     display: "grid",
-    gridGap: "1rem",
+    gridGap: ({ gapSize }) => `${gapSize}rem`,
     gridTemplateColumns: ({ level }) => `repeat(${level}, 1fr)`,
     border: "3px solid green",
+    "@media screen and (max-width: 575.98px)": {
+      width: "40vh",
+      height: "40vh",
+      gridGap: ({ gapSize }) => `${gapSize / 2.5}rem`,
+    },
   },
   card: {
-    backgroundColor: ({ color }) => `hsl(${color}, 50%, 50%)`,
+    backgroundColor: ({ color, principalColorSatAndLum }) =>
+      `hsl(${color}, ${principalColorSatAndLum}%, ${principalColorSatAndLum}%)`,
     color: "white",
   },
   cardDifferent: {
-    backgroundColor: ({ color }) => `hsl(${color}, 20%, 50%)`,
+    backgroundColor: ({ color, saturation, lightness }) =>
+      `hsl(${color}, ${saturation}%, ${lightness}%)`,
     color: "white",
   },
 });
 
 const Game = ({ level, onCardClicked }) => {
+  //Color region
+  const principalColorSatAndLum = 50;
   const [color, setColor] = useState();
-
-  useEffect(() => {
+  const [saturation, setSaturation] = useState(30);
+  const [lightness, setLightness] = useState(30);
+  const randomColors = () => {
+    const maxVariant = 20 - level / 2;
+    const diffVariant = Math.floor(Math.random() * maxVariant);
     setColor(Math.floor(Math.random() * 360));
+    setSaturation(principalColorSatAndLum - (maxVariant - diffVariant));
+    setLightness(principalColorSatAndLum - diffVariant);
+  };
+  useEffect(() => {
+    randomColors();
   }, [level]);
 
+  //Size and gameplay region
   const matrixSize = level * level;
   const nDifferent = Math.floor(Math.random() * matrixSize);
   const checkResult = (tileIndex) => () => {
     tileIndex === nDifferent ? onCardClicked("won") : onCardClicked("lost");
   };
-  const classes = useStyles({ level, color });
+  const gapSize = 1 - 0.02 * level;
+  const classes = useStyles({
+    level,
+    color,
+    principalColorSatAndLum,
+    saturation,
+    lightness,
+    gapSize,
+  });
 
   return (
     <div className={classes.cards}>
