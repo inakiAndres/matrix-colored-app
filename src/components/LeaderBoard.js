d1b62506f7c28152a6ad0e6bc6ada3feba298fa1
inakiAndres <inaki.andres.beltran@accenture.com>
2020-11-04 13:46:23 +0100

    -Leaderboard styles done

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -10,12 +10,15 @@ const useStyles = createUseStyles({
     marginTop: "10px",
   },
   column: {
-    paddingRight: "10px",
-    paddingLeft: "10px",
+    minWidth: "25px",
+    padding: 10,
+    "@media screen and (max-width: 575.98px)": {
+      padding: 5,
+    },
   },
   input: {
     border: "2px solid #555",
-    backgroundColor: "transparent",
+    backgroundColor: "divansparent",
     width: "50px",
     textAlign: "center",
   },
@@ -23,25 +26,36 @@ const useStyles = createUseStyles({
     margin: "0 10px",
     border: "2px solid #555",
     borderRadius: "8px",
-    backgroundColor: "transparent",
+    backgroundColor: "divansparent",
     textAlign: "center",
     fontFamily: "Itim",
     boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0)",
   },
   firstClassified: {
+    display: "flex",
+    justifyContent: "space-around",
     marginBottom: "10px",
     backgroundColor: "goldenrod",
     boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.16)",
   },
   secondClassified: {
+    display: "flex",
+    justifyContent: "space-around",
     marginBottom: "10px",
     backgroundColor: "silver",
     boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.16)",
   },
   thirdClassified: {
+    display: "flex",
+    justifyContent: "space-around",
     marginBottom: "10px",
     backgroundColor: "#cd7f32",
     boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.16)",
+  },
+  defaultClassified: {
+    display: "flex",
+    justifyContent: "space-around",
+    marginBottom: "10px",
   },
 });
 
@@ -92,44 +106,38 @@ const LeaderBoard = ({ level, onRestart }) => {
         </div>
       )}
       <div className={classes.space}>LEADERBOARD</div>
-      <table className={classes.space}>
-        <thead>
-          <tr>
-            <th className={classes.column}>Nickname</th>
-            <th className={classes.column}>Level</th>
-          </tr>
-        </thead>
-        <tbody>
-          {scores.map(
-            (score, index) =>
-              ({
-                0: (
-                  <tr className={classes.firstClassified} key={index}>
-                    <td className={classes.column}>{score.nick}</td>
-                    <td className={classes.column}>{score.level}</td>
-                  </tr>
-                ),
-                1: (
-                  <tr className={classes.secondClassified} key={index}>
-                    <td className={classes.column}>{score.nick}</td>
-                    <td className={classes.column}>{score.level}</td>
-                  </tr>
-                ),
-                2: (
-                  <tr className={classes.thirdClassified} key={index}>
-                    <td className={classes.column}>{score.nick}</td>
-                    <td className={classes.column}>{score.level}</td>
-                  </tr>
-                ),
-              }[index] || (
-                <tr key={index}>
-                  <td className={classes.column}>{score.nick}</td>
-                  <td className={classes.column}>{score.level}</td>
-                </tr>
-              ))
-          )}
-        </tbody>
-      </table>
+      <div className={classes.defaultClassified}>
+        <div className={classes.column}>Nickname</div>
+        <div className={classes.column}>Level</div>
+      </div>
+      {scores.map(
+        (score, index) =>
+          ({
+            0: (
+              <div className={classes.firstClassified} key={index}>
+                <div className={classes.column}>{score.nick}</div>
+                <div className={classes.column}>{score.level}</div>
+              </div>
+            ),
+            1: (
+              <div className={classes.secondClassified} key={index}>
+                <div className={classes.column}>{score.nick}</div>
+                <div className={classes.column}>{score.level}</div>
+              </div>
+            ),
+            2: (
+              <div className={classes.thirdClassified} key={index}>
+                <div className={classes.column}>{score.nick}</div>
+                <div className={classes.column}>{score.level}</div>
+              </div>
+            ),
+          }[index] || (
+            <div className={classes.defaultClassified} key={index}>
+              <div className={classes.column}>{score.nick}</div>
+              <div className={classes.column}>{score.level}</div>
+            </div>
+          ))
+      )}
     </>
   );
 };
