4520a6677980f4cd70c7886c10f0e01d977ae23c
inakiAndres <inaki.andres.beltran@accenture.com>
2020-11-05 20:00:05 +0100

    -Fixed color button menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -36,7 +36,7 @@ const useStyles = createUseStyles({
     padding: 10,
     margin: "20px",
     border: "1px solid",
-    backgroundColor: "divansparent",
+    backgroundColor: "white",
     textAlign: "center",
     fontFamily: "Itim",
     boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0)",
